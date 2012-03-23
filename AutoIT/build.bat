71ff7e57b1bd71748ccefcd631749c9e5db66eff
Xyrfo <xyrfox@gmail.com>
2012-03-24 00:58:21 +0200

    AutoIT build tool tweaks

--- AutoIT/build.bat
+++ AutoIT/build.bat
@@ -3,18 +3,22 @@
 :: Set to pack to use UPX compression
 set upx=nopack
 
+:: Set to /x64 to build for 64-bit
+set x64=
+
 :: AutoIt3Wrapper directory
 set autwrapperdir=%PROGRAMFILES(X86)%\AutoIt3\SciTE\AutoIt3Wrapper
 
 echo Building AutoIT extensions...
 
 :: Build scripts
-FOR /R "." %%G IN (*.au3) DO "%autwrapperdir%\AutoIt3Wrapper.exe" /in %%G /%upx%
+FOR %%G IN (*.au3) DO "%autwrapperdir%\AutoIt3Wrapper.exe" /in %%G /%upx% %x64%
 
-:: For ricers: uncomment the following line and comment the above to build for X64
-:: FOR /R "." %%G IN (*.au3) DO "%autwrapperdir%\AutoIt3Wrapper.exe" /in %%G /%upx% /x64
+:: Don't forget the Panel Creator!
+"%autwrapperdir%\AutoIt3Wrapper.exe" /in "PanelCreator\PanelCreator.au3" /%upx% %x64%
 
-:: Delete unnecessary exe
+:: Delete unnecessary exes
+del Common.exe
 del ColorChooser.exe
 
 echo All done!
